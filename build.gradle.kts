b40896b1498a1cb2aa049afb5bdf993f4ed6be10
rekkido <dikkerd@ya.ru>
2023-11-25 00:28:57 +0300

    Add Allure Report

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     application
+    id("io.qameta.allure") version "2.11.2"
 }
 
 group = "org.example"
@@ -16,6 +17,7 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
     implementation("io.github.serpro69:kotlin-faker:1.15.0")
+    implementation("io.qameta.allure:allure-junit-platform:2.24.0")
 }
 
 tasks.test {
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,7 +3,9 @@ import data.Task
 import data.TasksRepository
 import data.TasksRepositoryMemory
 import io.github.serpro69.kfaker.Faker
+import io.qameta.allure.Description
 import kotlin.test.BeforeTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -15,7 +17,7 @@ class MainTest {
     @BeforeTest
     fun populateTasks() {
         val tasksRepositoryMemory = TasksRepositoryMemory()
-        val iterations = faker.random.nextInt(2,5)
+        val iterations = faker.random.nextInt(2, 5)
         for (i in 1..iterations) {
             val name = faker.cowboyBebop.character()
             val priority = Priority.values().random()
@@ -25,30 +27,36 @@ class MainTest {
     }
 
     @Test
+    @Description("Добавление задачи и появление ее в списке")
     fun addTaskAndCheckAppearanceTest() {
         val name = faker.cowboyBebop.character()
         val priority = Priority.values().random()
         val newTaskId = testRepository.addTask(Task(name = name, priority = priority))
         val lastIdFromTaskList = testRepository.getTasks().last().id
-        assertEquals(newTaskId,lastIdFromTaskList)
+        assertEquals(newTaskId, lastIdFromTaskList)
     }
 
     @Test
+    @Description("Завершить задачу и проверить корректность работы фильтра по завершенным задачам")
     fun checkFinishFilterAfterFinishTaskTest() {
         val name = faker.cowboyBebop.character()
         val priority = Priority.values().random()
         val newTaskId = testRepository.addTask(Task(name = name, priority = priority))
         testRepository.completeTask(newTaskId)
         val listOfCompletedTasks = testRepository.getTasks(completed = true).size
-        assertEquals(1,listOfCompletedTasks,"Expected 1 task in filter, but got $listOfCompletedTasks")
+        assertEquals(1, listOfCompletedTasks, "Expected 1 task in filter, but got $listOfCompletedTasks")
     }
 
     @Test
+    @Ignore
+    @Description("Проверить сортировку задач по названию")
     fun checkSortingByNameTest() {
         // Sorting is not implemented yet
     }
 
     @Test
+    @Ignore
+    @Description("Проверить сортировку задач по сроку исполнения")
     fun checkSortingByDueTimeTest() {
         // Sorting is not implemented yet
     }
