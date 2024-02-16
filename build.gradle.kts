c306d8938004b8abf1a3d55676cfde914bfb6384
elenakruzhechkina <jam23@yandex.ru>
2024-02-16 19:11:26 +0300

    HW4 comit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("io.qameta.allure") version "2.11.2"
     application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val allureVersion = "2.24.0"
 
 repositories {
     mavenCentral()
@@ -15,6 +20,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-framework-engine:5.6.2")
+
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.18")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.18")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("io.kotest:kotest-framework-datatest:5.5.5")
+
+    testImplementation ("io.qameta.allure:allure-junit5:2.24.0")
 }
 
 tasks.test {
@@ -28,3 +43,4 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("MainKt")
 }
+
